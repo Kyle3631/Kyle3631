9508a2c5f46fed52f4bf3f9e5651945238ba9f59
apple <kyle921220kyle@gmail.com>
2021-07-24 22:51:00 +0800

    make it better

--- app.py
+++ app.py
@@ -150,6 +150,7 @@ def dramac():
 
 def movie777():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+    
     target_url = 'https://dogevod.com/browse/movies.html'
     print('Start drama ...')
     rs = requests.session()
